6e6dd7bb828c9a5ff5d3ebbfbf6164161fb3c47d
lc3lx <140909706+lc3lx@users.noreply.github.com>
2025-03-02 00:19:58 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AdminPage from './components/AdminPage';
 import ResidencePage from './components/UserPage';
 import PasswordResetPage from './components/SinglePage';
